99dc27394c24ca709c789aba9262c14f713382a9
Bilal Doganay <bilaldgny47@gmail.com>
2023-07-29 23:27:18 +0200

    Hello World

---
+++ src/day01_HelloWorld/C01_HelloWorld.java
@@ -0,0 +1,19 @@
+package day01_HelloWorld;
+
+public class C01_HelloWorld {
+    public static void main(String[] args) {
+
+        //Bu class yazilim dunyasina yeni giren herkesin yaptigi gibi
+        // Konsolda hello world yazdirmak icin olusturuldu
+        System.out.println("Hello World");
+
+        /*
+            Bu
+            sekilde
+            yazdigimizda
+            ikisi arasinda
+            yazilan tum satirlari
+            yorum olarak kabul eder
+         */
+    }
+}
